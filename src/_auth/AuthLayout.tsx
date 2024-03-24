a15fb5cb2bb8a65bb54cd88d37e3d598e17e685d
Kona <landon.deroin@gmail.com>
2024-03-24 15:00:52 -0400

    more setup

---
+++ src/_auth/AuthLayout.tsx
@@ -0,0 +1,5 @@
+const AuthLayout = () => {
+  return <div>Authlayout</div>;
+};
+
+export default AuthLayout;
---
+++ src/_auth/forms/SigninForm.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SigninForms = () => {
+  return <div>signin</div>;
+};
+
+export default SigninForms;
