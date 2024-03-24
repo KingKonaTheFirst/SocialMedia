2ff6db5cd40d703b4d1646c8a7ad64567d9e980f
Kona <landon.deroin@gmail.com>
2024-03-24 17:15:57 -0400

    signup form layout and intial zod form

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 
 const SigninForms = () => {
   return <div>signin</div>;
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,13 @@
-import * as ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import * as ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")!).render();
-<BrowserRouter>
-<App />
-</BrowserRouter>
+const rootElement = document.getElementById("root");
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+}
